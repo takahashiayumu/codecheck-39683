2794dd0206630664de3ce3a5d1072481b18c2e15
tie301884 <tie301884@S1703C15525-T1.tisnt.tis.co.jp>
2018-05-30 13:30:19 +0900

    commit

--- src/main/java/codecheck/App.java
+++ src/main/java/codecheck/App.java
@@ -5,6 +5,7 @@ public class App {
 		for (int i = 0, l = args.length; i < l; i++) {
 			String output = String.format("argv[%s]: %s", i, args[i]);
 			System.out.println(output);
+			// hogehogehoge
 		}
 	}
 }
